7412e0c243cf5fb6ee5daf9d69b91f51a2bbfee6
nelbom1234 <79920825+nelbom1234@users.noreply.github.com>
2025-04-08 11:04:09 +0200

    add units tests for 0

--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -19,9 +19,21 @@ public class Tests
     }
 
     [Test]
+    public void TestForZero() {
+        int result = new ForSum().CalculateSum(0);
+        Assert.That(result == 0);
+    }
+
+    [Test]
     public void TestArithmetic() {
         int result = new ArithmeticSum().CalculateSum(10);
         Assert.That(result == 55);
+    }
+
+    [Test]
+    public void TestArithmeticZero() {
+        int result = new ArithmeticSum().CalculateSum(0);
+        Assert.That(result == 0);
     }
 
     [Test]
@@ -31,14 +43,32 @@ public class Tests
     }
 
     [Test]
+    public void TestForEachZero() {
+        int result = new ForEachSum().CalculateSum(0);
+        Assert.That(result == 0);
+    }
+
+    [Test]
     public void TestWhile() {
         int result = new WhileSum().CalculateSum(15);
         Assert.That(result == 120);
     }
 
     [Test]
+    public void TestWhileZero() {
+        int result = new WhileSum().CalculateSum(0);
+        Assert.That(result == 0);
+    }
+
+    [Test]
     public void TestRecursive() {
         int result = new RecursiveSum().CalculateSum(9);
         Assert.That(result == 45);
+    }
+
+    [Test]
+    public void TestRecursiveZero() {
+        int result = new RecursiveSum().CalculateSum(0);
+        Assert.That(result == 0);
     }
 }
